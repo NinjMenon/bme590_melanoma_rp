0d349a250b3c8866ff44ed2c1da401c1d5606ae5
NinjMenon <ns229@duke.edu>
2017-12-05 00:18:44 -0500

    Tested - able to access images from usb, close #10

--- image_encoding.py
+++ image_encoding.py
@@ -8,13 +8,15 @@ Created on Sun Dec  3 13:00:36 2017
 import base64
 #import requests
 import glob
+import os
 
 filetypes = ('*.jpg', '*.tif', '*.png', '*.gif','*.jpx','*.pcd')
 c = 0 
 payload = {}
 image_list = []
+usb_dir = os.listdir('/media/pi/')[0]
 for files in filetypes:
-    image_list.extend(glob.glob('/media/usb/{}'.format(files)))
+    image_list.extend(glob.glob('/media/pi/{}/{}'.format(usb_dir,files)))
 
 for image in image_list:
     with open(image, 'rb') as im:
