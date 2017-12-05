d8fcb604f4307e7b6c3cf5e492da67553a0ce5da
NinjMenon <ns229@duke.edu>
2017-12-05 11:35:11 -0500

    Added try and except

--- image_encoding.py
+++ image_encoding.py
@@ -9,23 +9,28 @@ Created on Sun Dec  3 13:00:36 2017
 import base64
 import requests
 import glob
-#import os
+import os
 
 filetypes = ('*.jpg', '*.tif', '*.png', '*.gif','*.jpx','*.pcd')
 c = 0 
 payload = {}
 image_list = []
-#usb_dir = os.listdir(r'C:\Users\Niranjana\Documents\Independent Study\Data')[0]
-for files in filetypes:
-    image_list.extend(glob.glob(r'C:\Users\Niranjana\Documents\Independent Study\Data\Curve fitting\{}'.format(files)))
+try:
+    usb_dir = os.listdir('/media/pi/')[0]
+except IndexError:
+    usb_dir =[]
 
-for image in image_list:
-    with open(image, 'rb') as im:
-        im_read = im.read()
-        im_encode = base64.encodebytes(im_read)
-        key = 'image' + str(c)
-        payload[key] = im_encode
-        c = c+1
+if usb_dir:
+    for files in filetypes:
+        image_list.extend(glob.glob('/media/pi/{}/{}'.format(usb_dir,files)))
 
-if payload:
-    r = requests.post('http://vcm-1854.vm.duke.edu:5000/image_classified_result', data=payload) 
+    for image in image_list:
+        with open(image, 'rb') as im:
+            im_read = im.read()
+            im_encode = base64.encodebytes(im_read)
+            key = 'image' + str(c)
+            payload[key] = im_encode
+            c = c+1
+
+    if payload:
+        r = requests.post('http://vcm-1854.vm.duke.edu:5000/image_classified_result', data=payload) 
