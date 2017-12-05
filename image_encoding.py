5fc2d5e12369e009b248fcab3b02330e36da962c
NinjMenon <ns229@duke.edu>
2017-12-04 23:51:47 -0500

    Working code - reads all img files, encodes, puts into dictionary for sending

--- image_encoding.py
+++ image_encoding.py
@@ -6,19 +6,22 @@ Created on Sun Dec  3 13:00:36 2017
 """
 
 import base64
-import requests
+#import requests
 import glob
 
-filetypes = ['.jpg', '.tif', '.png', '.gif','.jpx','.pcd']
+filetypes = ('*.jpg', '*.tif', '*.png', '*.gif','*.jpx','*.pcd')
 c = 0 
 payload = {}
-# For Raspberry Pi, change the folder below to media/usb
-for image in glob.glob("folder/*."):
-    if image.endswith(t for t in filetypes):
-        with open(image) as im:
-            im_read = im.read()
-            im_encode = base64.encodebytes(im_read)
-            key = 'image' + str(c)
-            payload[key] = im_encode
-            
-r = requests.post('http://http.org/post', data=payload) # placeholder 
+image_list = []
+for files in filetypes:
+    image_list.extend(glob.glob('/media/usb/{}'.format(files)))
+
+for image in image_list:
+    with open(image, 'rb') as im:
+        im_read = im.read()
+        im_encode = base64.encodebytes(im_read)
+        key = 'image' + str(c)
+        payload[key] = im_encode
+        c = c+1
+        
+#r = requests.post('http://http.org/post', data=payload) # placeholder 
